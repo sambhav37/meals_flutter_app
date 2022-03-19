2c273eafab7442086e61b987473959d3f211ef18
kusambhav <kusambhav@expediagroup.com>
2022-03-19 15:28:12 +0530

    Adding images view support

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_complete_guide/category_item.dart';
 
-import './dummy_data.dart';
+import '../widgets/category_item.dart';
+import '../dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
